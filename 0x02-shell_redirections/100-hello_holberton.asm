d86ac4130759041ae8ba0ac5202f1a08c8af0c0f
StaciAF <aaen.it19@gmail.com>
2020-03-16 05:29:00 +0000

    adds 64-bit program in assembly that prints

--- 0x02-shell_redirections/100-hello_holberton.asm
+++ 0x02-shell_redirections/100-hello_holberton.asm
@@ -1,13 +1,32 @@
-section .data
+extern printf
 
-	    message db "Hello, Holberton\n", 10
+	segment .data
+	    msg db     "Hello, Holberton", 0x0
+	    len equ     $ - msg
+formatstr:
+	              dd formatstring
 
-	section .text
+	segment .rodata
+formatstring:	 db 25H, 73H, 0AH, 00H
 
-	global _start
-_start:
-	    mov rax, 1
-	    mov rdi, 1
-	    mov rsi, message
-	    mov rdx, 14
-	    syscall
+
+	segment .text
+	    global  main
+
+printstring:
+	    push    dword [esp + 4]
+	    push    dword [formatstr]
+	    call    printf
+	    add     esp, 8
+	    ret
+
+main:
+	    enter 0,0
+	    pusha
+	    push msg
+	    call printstring
+
+	    popa
+	    xor eax, eax
+	    leave
+	    ret
