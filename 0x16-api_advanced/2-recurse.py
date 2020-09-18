ac59bfde197341b6c14bd17970bb45693ae4650c
StaciAF <aaen.it19@gmail.com>
2020-09-18 04:47:46 +0000

    still not right

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -6,18 +6,15 @@ import json
 import requests
 
 
-def recurse_you(subreddit, hot_list=[]):
+def recurse(subreddit, hot_list=[]):
     """ this function queries reddit for 10 hot posts of given subreddit """
-    reddit = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
+    reddit = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
     u_head = {'User-Agent': 'Stacipoo'}
     red_req = requests.get(reddit, headers=u_head, allow_redirects=False)
     if red_req.status_code is not 200:
         return ('None')
     red_json = red_req.json()
-    key = 'data'
-    if key in red_json:
-        data = red_json.get(key)
-        chil = data.get('children')
-    for info in chil:
-        hot_title = info.get("data").get("title")
-        print(hot_title)
+    if 'after' in red_json["meta"]["pagination"]:
+        after = red_json["meta"]["pagination"]["after"]
+        hot_list.append(after)
+    return (recurse(subreddit, hot_list=[]))
