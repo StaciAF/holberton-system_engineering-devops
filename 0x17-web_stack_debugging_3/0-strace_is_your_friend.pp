e396c3eeb9d5238ca532210c9996ecb578cf6101
StaciAF <aaen.it19@gmail.com>
2020-09-30 23:28:31 +0000

    adds strace puppet file, ran in provided container

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 #puppet manifest to resolve WP 500 error
 
-exec { 'modify php extension'
-  command => "sed -i 's/phpp/php/g' /var/www/html/wp-includes/class-wp-locale.phpp"
+exec { 'modify php extension':
+  command => "sed -i 's/phpp/php/g' /var/www/html/wp-settings.php",
   path    => '/bin'
 }
